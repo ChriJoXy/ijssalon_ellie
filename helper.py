5a75593ee562a622ecab8f84bb3a94b0c6db9981
Christy-Ann Sambo <christyann.sambo@gmail.com>
2024-07-18 11:01:03 -0400

    bestand tijdelijk.py gecodeerd

--- helper.py
+++ helper.py
@@ -1,7 +1,1 @@
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
+print("20" + "20")
---
+++ test7.py
@@ -0,0 +1,10 @@
+prijzen ={
+"aardbei" :3,
+"vanille" :4,
+"chocolade" :5
+}
+aanbieding =prijzen["aardbei"]*0.8
+reclame_tekst= f"Vandaag in de aanbieding vanille-ijs, 1 liter slechts € {aanbieding}"
+reclame_tekst2 =reclame_tekst
+reclame_tekst3 =(reclame_tekst).upper()
+print(reclame_tekst3[:60])
